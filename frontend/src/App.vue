b7a8e9dff1cb90b1dcf498adacdebbd9ff8221ba
Sacha BOUDHOU <72568998+chubibyy@users.noreply.github.com>
2023-07-09 20:49:37 +0200

    Add read folder and clean name with regex

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <div>
-    <AnimeCard  
-      :title="animes.title"
-      :score="animes.score"
-      :image="animes.images?.jpg?.image_url"
-    />
+  <div id="app">
+    <input type="file" @change="handleFileUpload" multiple webkitdirectory directory />
+    <ul>
+      <li v-for="(folder, index) in folders" :key="index">{{ folder }}</li>
+    </ul>
+    <div v-for="(anime, index) in animes" :key="index">
+      <AnimeCard  
+        :title="anime.title"
+        :score="anime.score"
+        :image="anime.images?.jpg?.image_url"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import AnimeCard from './components/AnimeCard.vue'
+
 export default {
   name : 'App',
   components: {
@@ -17,18 +24,45 @@ export default {
   },
   data() {
     return {
-      animes: []
+      animes: [],
+      folders: []
     }
   },
-  // Appel API pour récupérer les animes
-  created() {
-    fetch(`${process.env.VUE_APP_API_URL}/anime/konosuba`)
-      .then(response => response.json())
-      .then(data => {
-        this.animes = data;
-        console.log(this.animes.title)
+  methods: {
+    async handleFileUpload(event) {
+      const files = [...event.target.files];
+      const folderSet = new Set();
+      files.forEach(file => {
+        let pathParts = file.webkitRelativePath.split('/');
+        let folder = pathParts.slice(1, pathParts.length - 1).join('/');
+        const slashIndex = folder.indexOf('/');
+        if (slashIndex > -1) {
+          folder = folder.substring(0, slashIndex);
+        }
+        // Supprimer tout ce qui vient après le nom principal de l'anime
+        const regex = /(\s\d+|\sS\d+|\sVOSTFR|\sMULTi|\sBD|\sHDTV|\sWEBRip|\sWEB-DL|\sAAC|\sWKN|\sBDRip|\sBDRIP|\sFLAC|\sX265|\s10BITS|\s720p|\s1080p|\s-\s|[[(]|\.[\w\d-]+)/i;
+        const matchIndex = folder.search(regex);
+        if (matchIndex > -1) {
+          folder = folder.substring(0, matchIndex).trim();
+        }
+        folderSet.add(folder);
+      });
+      this.folders = Array.from(folderSet);
+      await this.fetchAnimes();
+    }
 
-      })
+,
+    async fetchAnimes() {
+      for(const folder of this.folders){
+        try {
+          const response = await fetch(`${process.env.VUE_APP_API_URL}/anime/${encodeURIComponent(folder)}`)
+          const data = await response.json();
+          this.animes.push(data);
+        } catch (error) {
+          console.error('There was an error!', error);
+        }
+      }
+    }
   }
 }
 </script>
