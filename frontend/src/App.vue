cbf496d6e65769039555671e1ba51391495f560f
Sacha BOUDHOU <72568998+chubibyy@users.noreply.github.com>
2023-07-09 16:44:58 +0200

    frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js Apsp" />
+    <button @click="getData">Get Data</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    getData() {
+      fetch(`${process.env.VUE_APP_API_URL}/data`)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+        });
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
